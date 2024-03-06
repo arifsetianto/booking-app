b3e15253abe953f53e1dfe97860a63d2e5f49209
arifsetianto <arifsetiantoo@gmail.com>
2024-03-06 23:10:15 +0700

    enhance: change cover product card

--- resources/views/livewire/pages/products/list-products.blade.php
+++ resources/views/livewire/pages/products/list-products.blade.php
@@ -9,7 +9,7 @@ use function Livewire\Volt\{state};
 <div>
     <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="{{ route('orders.book') }}">
-            <img class="pb-8 rounded-t-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/featured/image.jpg" alt="product image" />
+            <img class="pb-8 rounded-t-lg" src="{{ asset('images/product-1.jpg') }}" alt="product image" />
         </a>
         <div class="px-5 pb-5">
             <a href="{{ route('orders.book') }}">
