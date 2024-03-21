f20cb37afc35e83274750b13ec5d60672cf83d71
arifsetianto <arifsetiantoo@gmail.com>
2024-03-21 23:11:57 +0700

    fix: remove validation mimes image

--- app/Livewire/Forms/Order/ConfirmPaymentForm.php
+++ app/Livewire/Forms/Order/ConfirmPaymentForm.php
@@ -13,6 +13,6 @@ use Livewire\Form;
  */
 class ConfirmPaymentForm extends Form
 {
-    #[Validate('required|image|mimes:jpg,jpeg,png|max:5120', message: ['max' => 'Maximum upload file size is 5MB'])]
+    #[Validate('required|image|max:5120', message: ['max' => 'Maximum upload file size is 5MB'])]
     public TemporaryUploadedFile $receiptFile;
 }
