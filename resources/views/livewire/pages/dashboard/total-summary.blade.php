aa879d1fc8e55430db7439a09574e24c7bf63bbb
arifsetianto <arifsetiantoo@gmail.com>
2024-03-31 04:13:19 +0700

    enhance: add total summary in dashboard

--- resources/views/livewire/pages/dashboard/total-summary.blade.php
+++ resources/views/livewire/pages/dashboard/total-summary.blade.php
@@ -8,26 +8,45 @@ use function Livewire\Volt\{state};
 
 new class extends Component {
     public int $totalRegisteredUsers = 0;
+    public int $totalUsersOrdered = 0;
     public int $totalIncome = 0;
+    public int $totalPendingOrders = 0;
     public int $totalIncomingOrders = 0;
     public int $totalVerifiedOrders = 0;
     public int $totalCompletedOrders = 0;
+    public int $totalRevisedOrders = 0;
 
     public function mount(): void
     {
         $this->totalRegisteredUsers = User::whereNotNull('profile_id')->count();
+        $this->totalUsersOrdered =
+            User::whereNotNull('profile_id')->whereIn(
+                'id',
+                Order::select('user_id')->whereIn(
+                    'status',
+                    [
+                        OrderStatus::PENDING,
+                        OrderStatus::CONFIRMED,
+                        OrderStatus::VERIFIED,
+                        OrderStatus::COMPLETED,
+                        OrderStatus::REVISED
+                    ]
+                )->get()
+            )->count();
         $this->totalIncome = Order::whereIn('status', [OrderStatus::VERIFIED, OrderStatus::COMPLETED])->sum('amount');
+        $this->totalPendingOrders = Order::where('status', OrderStatus::PENDING)->count();
         $this->totalIncomingOrders = Order::where('status', OrderStatus::CONFIRMED)->count();
         $this->totalVerifiedOrders = Order::where('status', OrderStatus::VERIFIED)->count();
         $this->totalCompletedOrders = Order::where('status', OrderStatus::COMPLETED)->count();
+        $this->totalRevisedOrders = Order::where('status', OrderStatus::REVISED)->count();
     }
 };
 
 ?>
 
 <div>
-    <div class="grid grid-cols-1 md:grid-cols-5 gap-4">
-        <div class="max-w-sm w-full bg-white shadow rounded-lg dark:bg-gray-800 p-4 md:p-6">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 pb-3">
+        <div class="max-w-xl w-full bg-white shadow rounded-lg dark:bg-gray-800 p-4 md:p-6">
             <div class="flex justify-center items-center">
                 <div class="text-center">
                     <h5 class="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">{{ number_format($totalRegisteredUsers) }}</h5>
@@ -35,11 +54,30 @@ new class extends Component {
                 </div>
             </div>
         </div>
+        <div class="max-w-xl w-full bg-white shadow rounded-lg dark:bg-gray-800 p-4 md:p-6">
+            <div class="flex justify-center items-center">
+                <div class="text-center">
+                    <h5 class="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">{{ number_format($totalUsersOrdered) }}</h5>
+                    <p class="text-base font-normal text-gray-500 dark:text-gray-400">Total Users Ordered</p>
+                </div>
+            </div>
+        </div>
+        <div class="max-w-xl w-full bg-white shadow rounded-lg dark:bg-gray-800 p-4 md:p-6">
+            <div class="flex justify-center items-center">
+                <div class="text-center">
+                    <h5 class="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">
+                        ฿ {{ number_format($totalIncome) }}</h5>
+                    <p class="text-base font-normal text-gray-500 dark:text-gray-400">Total Income</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-5 gap-4 pt-3">
         <div class="max-w-sm w-full bg-white shadow rounded-lg dark:bg-gray-800 p-4 md:p-6">
             <div class="flex justify-center items-center">
                 <div class="text-center">
-                    <h5 class="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">฿ {{ number_format($totalIncome) }}</h5>
-                    <p class="text-base font-normal text-gray-500 dark:text-gray-400">Total Income</p>
+                    <h5 class="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">{{ number_format($totalPendingOrders) }}</h5>
+                    <p class="text-base font-normal text-gray-500 dark:text-gray-400">Total Pending Orders</p>
                 </div>
             </div>
         </div>
@@ -64,6 +102,14 @@ new class extends Component {
                 <div class="text-center">
                     <h5 class="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">{{ number_format($totalCompletedOrders) }}</h5>
                     <p class="text-base font-normal text-gray-500 dark:text-gray-400">Total Completed Orders</p>
+                </div>
+            </div>
+        </div>
+        <div class="max-w-sm w-full bg-white shadow rounded-lg dark:bg-gray-800 p-4 md:p-6">
+            <div class="flex justify-center items-center">
+                <div class="text-center">
+                    <h5 class="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">{{ number_format($totalRevisedOrders) }}</h5>
+                    <p class="text-base font-normal text-gray-500 dark:text-gray-400">Total Revised Orders</p>
                 </div>
             </div>
         </div>
